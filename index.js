facc5eaf2f5c30782c08adb7c510e26b9cfb5955
rdalin82 <Robert.Dalin@gmail.com>
2016-05-19 22:53:57 -0500

    ready for github

--- index.js
+++ index.js
@@ -6,12 +6,25 @@ app.use(express.static('public'));
 app.get('/', function(req, res){
 	res.sendFile(__dirname + "/views/index.html");
 });
-
+var msgList = [];
 io.on('connection', function(socket){
+	msgList = msgList.reverse();
+	msgList = msgList.slice(0, 9);
+	msgList = msgList.reverse();
+	msgList.forEach(function(m) {
+		socket.emit('chat message', m);
+	});
+	
 	console.log('a user connected');
 	socket.on('chat message', function(msg){
-		console.log('message: ' + msg);
-		io.emit('chat message', msg);
+		if (msg.length <= 0){
+			socket.emit('chat message', "message cannot contain nothing");
+		} else {
+			console.log('message: ' + msg);
+			io.emit('chat message', msg);
+			msgList.push(msg);
+		}
+
 	});
 	socket.on('disconnect', function(socket){
 		console.log('user disconnected');
